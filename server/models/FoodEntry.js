6818de44a5475227a25e10ab182e933b7f250fb3
tylerolszewski <tylerjolszewski@gmail.com>
2024-10-20 16:41:18 -0500

    Added Schemas

---
+++ server/models/FoodEntry.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+// Define Food Entry Schema
+const foodEntrySchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  calories: {
+    type: Number,
+    required: true,
+  },
+  carbohydrates: {
+    type: Number,
+    required: true,
+  },
+  proteins: {
+    type: Number,
+    required: true,
+  },
+  fats: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('FoodEntry', foodEntrySchema);
