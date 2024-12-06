96505fac30a856951a984bdce81a3b80271dc2a5
tylerolszewski <tylerjolszewski@gmail.com>
2024-12-06 00:28:48 -0600

    Added Deletion

--- server/routes/foodEntries.js
+++ server/routes/foodEntries.js
@@ -9,7 +9,7 @@ router.post('/add', auth, async (req, res) => {
 
   try {
     const foodEntry = new FoodEntry({
-      user: req.user.id,  // Get user ID from the token
+      user: req.user.id,
       name,
       calories,
       carbohydrates,
@@ -30,6 +30,23 @@ router.get('/', auth, async (req, res) => {
   try {
     const foodEntries = await FoodEntry.find({ user: req.user.id });
     res.json(foodEntries);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+router.delete('/remove/:id', auth, async (req, res) => {
+  try {
+    // Ensure the entry belongs to the logged-in user
+    const foodEntry = await FoodEntry.findOne({ _id: req.params.id, user: req.user.id });
+    
+    if (!foodEntry) {
+      return res.status(404).json({ msg: 'Food entry not found' });
+    }
+
+    await foodEntry.deleteOne();
+    res.json({ msg: 'Food entry removed' });
   } catch (err) {
     console.error(err);
     res.status(500).send('Server error');
